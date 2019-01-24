2669903ee38c2dbc7b2f286c6c0d97ccb0b86d85
dhershman1 <dustinh17@gmail.com>
2019-01-24 17:05:48 -0500

    Cleaned up and added onto template engine

--- engine.js
+++ engine.js
@@ -1,9 +1,12 @@
 const { concat, reduce } = require('kyanite')
 const _curry3 = require('./_internals/_curry3')
 
+function applyAttr (attr, acc) {
+  return reduce(([k, v], html) => concat(` ${k}="${v}"`, html), acc, Object.entries(attr))
+}
+
 const compile = _curry3((name, attr, children) => {
-  const el = concat('>', reduce(([k, v], html) =>
-    concat(` ${k}="${v}"`, html), `<${name}`, Object.entries(attr)))
+  const el = concat('>', applyAttr(attr, `<${name}`))
 
   return concat(`</${name}>`, reduce(concat, el, children))
 })
@@ -12,10 +15,27 @@ function text (val) {
   return val
 }
 
+function img (attr) {
+  return concat(' />', applyAttr(attr, `<${name}`))
+}
+
 module.exports = {
-  text,
   compile,
-  main: compile('main'),
+  img,
+  text,
+  div: compile('div'),
   h1: compile('h1'),
-  p: compile('p')
+  h2: compile('h2'),
+  h3: compile('h3'),
+  h4: compile('h4'),
+  h5: compile('h5'),
+  h6: compile('h6'),
+  main: compile('main'),
+  p: compile('p'),
+  table: compile('table'),
+  thead: compile('thead'),
+  tbody: compile('tbody'),
+  td: compile('td'),
+  th: compile('th'),
+  tr: compile('tr')
 }
