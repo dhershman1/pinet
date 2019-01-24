7c0bcdbc0f9ffb29b25d972e9b65c103b0a4adf6
dhershman1 <dustinh17@gmail.com>
2019-01-24 17:06:50 -0500

    added testing data

---
+++ example/fixture/tmp.js
@@ -0,0 +1,9 @@
+/**
+ * @name tmp
+ * @description Add one to a value
+ * @param {Number} [a=1] A number to pass to add one to
+ * @return {Number} A number plus one
+ */
+function tmp (a = 1) {
+  return a + 1
+}
