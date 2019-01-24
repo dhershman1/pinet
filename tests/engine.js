d7d20408ad573ca5e36a1f76e9cfd827d5209eca
dhershman1 <dustinh17@gmail.com>
2019-01-24 15:37:51 -0500

    Removed console log

--- tests/engine.js
+++ tests/engine.js
@@ -2,8 +2,6 @@ const test = require('tape')
 const { main, h1, text } = require('../engine')
 
 test('engine -- handles basic html types', t => {
-  console.log(h1({ data: 'foo', id: 'bar' }, [text('Pretty Pony')]))
-
   t.same(main({ data: 'foo', id: 'bar' }, []), '<main data="foo" id="bar"></main>')
   t.same(h1({ data: 'foo', id: 'bar' }, []), '<h1 data="foo" id="bar"></h1>')
   t.end()
