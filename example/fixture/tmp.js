4ec4e530ca871d01a028f60528279f7b4433aa31
dhershman1 <dustinh17@gmail.com>
2019-01-25 14:32:18 -0500

    testing out multi type in examples

--- example/fixture/tmp.js
+++ example/fixture/tmp.js
@@ -1,7 +1,8 @@
 /**
  * @name tmp
+ * @function
  * @description Add one to a value
- * @param {Number} [a=1] A number to pass to add one to
+ * @param {Number|String|Array} [a=1] A number to pass to add one to
  * @return {Number} A number plus one
  */
 function tmp (a = 1) {
