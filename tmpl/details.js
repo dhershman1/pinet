64a00d5f206b728b6e4808e2e628f60c256c13c4
dhershman1 <dustinh17@gmail.com>
2019-02-11 16:42:41 -0500

    Clean up syntax a little bit for details

--- tmpl/details.js
+++ tmpl/details.js
@@ -4,13 +4,16 @@ const { dl, dt, dd, text } = require('../engine')
 function details (customTags = [], doclet) {
   const list = ['since', ...customTags]
 
-  return dl({ class: 'details' }, map(name => {
-    return concat(dd({ class: 'details__data' }, [
-      text(doclet[name])
-    ]), dt({ class: 'details__tag' }, [
-      text(`${capitalize(name)}:`)
-    ]))
-  }, list))
+  return dl({ class: 'details' }, map(name =>
+    concat(
+      dd({ class: 'details__data' }, [
+        text(doclet[name])
+      ]),
+      dt({ class: 'details__tag' }, [
+        text(`${capitalize(name)}:`)
+      ])
+    )
+  , list))
 }
 
 module.exports = details
