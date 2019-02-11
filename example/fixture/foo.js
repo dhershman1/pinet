45c5f28f7b308e96312236bb79bcff6d3b270c07
dhershman1 <dustinh17@gmail.com>
2019-02-11 15:43:39 -0500

    added new example test

---
+++ example/fixture/foo.js
@@ -0,0 +1,12 @@
+
+/**
+ * @name foo
+ * @function
+ * @description A foo subtractor
+ * @param {Number} a The number to subtract from
+ * @param {Number} b The number to subtract with
+ * @return {Number} The remainder
+ */
+function foo (a, b) {
+  return a - b
+}
