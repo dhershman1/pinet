5ee8945c2670b6535dec72d05814fd53ba13346b
dhershman1 <dustinh17@gmail.com>
2019-02-11 15:44:02 -0500

    Added meta capabilities

--- tmpl/layout.js
+++ tmpl/layout.js
@@ -1,8 +1,20 @@
 const { concat } = require('kyanite')
-const { compile } = require('../engine')
+const { compile, meta } = require('../engine')
+
+function buildMeta (metaArr) {
+  if (!metaArr) {
+    return []
+  }
+
+  return metaArr.map(meta)
+}
 
 function layout (opts, children = []) {
   return concat(compile('html', { lang: opts.lang || 'en' }, [
+    compile('head', {}, [
+      ...buildMeta(opts.meta),
+      compile('title', {}, opts.title || 'Documentation')
+    ]),
     compile('body', { id: 'root' }, children)
   ]), '<!DOCTYPE html>')
 }
