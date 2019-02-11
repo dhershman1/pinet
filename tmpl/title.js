7fd86cf202213c812bcdea6e30c7cc91a225834f
dhershman1 <dustinh17@gmail.com>
2019-02-11 15:43:30 -0500

    Created new title tmpl

---
+++ tmpl/title.js
@@ -0,0 +1,23 @@
+const { h1, span, text } = require('../engine')
+const { addIndex, concat, reduce, map, prop } = require('kyanite')
+
+function paramCommaList (params) {
+  const _reduce = addIndex(reduce)
+
+  return _reduce((str, acc, i) => concat(`${str}${i < params.length - 1 ? `, ` : ''}`, acc), '', params)
+}
+
+function title ({ name, params, returns }) {
+  return h1({ class: 'title' }, [
+    text(name),
+    span({ class: 'title__params' }, [
+      text('('),
+      paramCommaList(map(prop('name'), params)),
+      text(')')
+    ]),
+    text(' → '),
+    text(`{${paramCommaList(returns[0].type.names)}}`)
+  ])
+}
+
+module.exports = title
