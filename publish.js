7f2200de17f67d2a9587d78e3ec870f7f1681403
dhershman1 <dustinh17@gmail.com>
2024-10-08 10:32:43 -0400

    Added: Step logging for verbose

--- publish.js
+++ publish.js
@@ -8,6 +8,7 @@ const engine = require('./engine')
 const layout = require('./tmpl/layout')
 const container = require('./tmpl/container')
 const source = require('./tmpl/source')
+const log = require('./_internals/_log')
 
 /* global env */
 
@@ -20,10 +21,11 @@ function publish (taffyData, opts) {
   console.time('Time')
   // A simple destination function
   const dest = file => path.join(opts.destination, file)
+  const _log = log(opts.verbose)
   // Prune Template data using jsdoc helper
   const data = helper.prune(taffyData)
   // Build up pinet options
-  const pinet = { classes: {}, ...env.conf.pinet, hasHome: Boolean(opts.readme) }
+  const pinet = { classes: {}, ...env.conf.pinet, verbose: opts.verbose, hasHome: Boolean(opts.readme) }
   const children = []
   const navList = []
   const sources = []
@@ -31,6 +33,8 @@ function publish (taffyData, opts) {
   const render = layout(pinet)
   let pkg = null
 
+  _log('Starting Write...')
+  _log('Destination:', opts.destination)
   // Generate the destination folder
   fs.mkdirp(opts.destination)
     // Loop through documentation data
@@ -38,19 +42,23 @@ function publish (taffyData, opts) {
       data().each(doclet => {
         // If not package.json then build out the child element and add it to the nav list
         if (doclet.kind !== 'package') {
+          _log('Processing:', doclet.name)
           navList.push({ name: doclet.name, cat: doclet.category })
           children.push(container(pinet, doclet))
           // If we are generating source files make sure we do that too
           if (pinet.genSources) {
+            _log('Generating Source:', doclet.name)
             sources.push(source(doclet, opts))
           }
         // Otherwise its the package json data so set that
         } else {
+          _log('Found Package JSON...')
           pkg = doclet
         }
       })
 
       // Start writing the rendered out documentation html into an html file
+      _log('Writing Main Documentation HTML...')
       return fs.writeFile(dest('documentation.html'), render(children, navList, {
         pkg,
         changelog: pinet.changelog
@@ -58,38 +66,49 @@ function publish (taffyData, opts) {
     })
     // Run each source code function we generated and added to our array
     // This is an array of promises generated above to build source html
-    .then(() =>
-      Promise.all(sources))
+    .then(() => {
+      _log('Running Source Code Generation...')
+
+      return Promise.all(sources)
+    })
     // Once we run through all of our promises then go ahead and start writing them all out
     // Onto rendered HTML files
-    .then(sourceHtml =>
-      Promise.all(sourceHtml.map(({ name, html }) =>
+    .then(sourceHtml => {
+      _log('Writing Source Code HTML Files...')
+      return Promise.all(sourceHtml.map(({ name, html }) =>
         fs.writeFile(path.join(opts.destination, `${name}.html`), render(html, navList, {
           pkg,
           changelog: pinet.changelog
         }))))
-    )
+    })
     // Create the primary index html for the main home page and write it
-    .then(() =>
+    .then(() => {
+      _log('Writing Index HTML...')
       fs.writeFile(dest('index.html'), render([], navList, {
         pkg,
         changelog: pinet.changelog
-      }, opts.readme)))
+      }, opts.readme))
+    })
     // Copy over all the static files into our docs/static folder
-    .then(() =>
-      fs.copy(path.join(__dirname, 'static'), dest('static')))
+    .then(() => {
+      _log('Copying Static Files...')
+      return fs.copy(path.join(__dirname, 'static'), dest('static'))
+    })
     // If the user provided a changelog location then go ahead and read the data
     .then(() => {
       if (pinet.changelog) {
+        _log('Reading Changelog...')
         return fs.readFile(pinet.changelog, 'UTF-8')
       }
 
+      _log('No Changelog Found Skipping...')
       return false
     })
     // Take the changelog data and format it into html using the marked package
     // Then take that and write the changelog html file
     .then(data => {
       if (data) {
+        _log('Writing Changelog HTML...')
         return fs.writeFile(dest('changelog.html'), render([], navList, {
           pkg,
           changelog: pinet.changelog
